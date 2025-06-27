11d6ad9cde3033316e14a25c81f561287169ae5b
Mika <mika.weske@protonmail.com>
2025-06-27 17:13:20 +0200

    editing the trancactions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import supabase
 import platform
+import time
 
 
 load_dotenv()
@@ -43,12 +44,27 @@ def style(word):
     final_string = "<" + centered_string_content + ">"
     print(final_string)
 
-
 def clear():
     if operaingsystem == "Windows":
         os.system("cls")
     else:
         os.system("clear")
+
+def search_user(user):
+    try:
+        user = "projektaktien+" + user + "@gmail.com"
+        response = (
+        supabase.schema("auth")
+        .table("users")
+        .select("id")
+        .eq("email", user)
+        .execute()
+        )
+        response = response.data.id
+        return response
+    except Exception as e:
+        print("User not found")
+        print(e)
 
 def fetch_amount():
     tabellenname = "transaction"
@@ -57,28 +73,54 @@ def fetch_amount():
     try:
         empf_response = supabase.table(tabellenname).select("amount").eq("userid_empf", userid).execute()
         send_response = supabase.table(tabellenname).select("amount").eq("userid_send", userid).execute()
-        # Überprüfen, ob Daten vorhanden sind
-        if send_response.data:
-            send_amounts = [item["amount"] for item in send_response.data if "amount" in item and isinstance(item["amount"], (int, float))]
-        else:
-            if debugv == 1:
-                print(f"Keine Daten für User-ID '{userid}' in Tabelle '{tabellenname}' gefunden.")
-            return 0.0
-        if send_response.data:
-            empf_amounts = [item["amount"] for item in empf_response.data if "amount" in item and isinstance(item["amount"], (int, float))]
-
-        else:
-            if debugv == 1:
-                print(f"Keine Daten für User-ID '{userid}' in Tabelle '{tabellenname}' gefunden.")
-            return 0.0
-        total_amount = sum(empf_amounts) - sum(send_amounts)
-        return total_amount
-
     except Exception as e:
         print(f"Fail to load Data")
         if debugv == 1:
             print(e)
         return 0.0
+    if send_response.data:
+        send_amounts = [item["amount"] for item in send_response.data if "amount" in item and isinstance(item["amount"], (int, float))]
+        send_amounts = sum(send_amounts)
+    else:
+        if debugv == 1:
+            print(f"Keine Daten für User-ID '{userid}' in Tabelle '{tabellenname}' gefunden.")
+        send_amounts = 0.0
+    if empf_response.data:
+        empf_amounts = [item["amount"] for item in empf_response.data if "amount" in item and isinstance(item["amount"], (int, float))]
+        empf_amounts = sum(empf_amounts)
+    else:
+        if debugv == 1:
+            print(f"Keine Daten für User-ID '{userid}' in Tabelle '{tabellenname}' gefunden.")
+        empf_amounts = 0.0
+    total_amount = empf_amounts - send_amounts
+    return total_amount
+
+def make_tracation():
+    clear()
+    style("Send")
+    print("To were would you like to send the Money\n Please enter the name")
+    id = search_user(input())
+    if id == 1:
+        return 2
+    else:
+        print("How much money would you like to send?")
+        ammount = float(input())
+        if ammount <= fetch_amount():
+            try:
+                rep = (supabase.table()
+                    .insert({"amount": ammount,"userid_empf":id})
+                    .execute())
+                print("Sucsessful Send")
+                return 1
+            except Exception as e:
+                if debugv == 1:
+                    print(e)
+                    return 1
+        else:
+            print("You didnt have enough money")
+            return 1
+            time.sleep(5)
+
 def auth():
     global credits_response
     global userid
@@ -103,8 +145,9 @@ def auth():
             return 1
         except Exception as e:
             print(f"Fail to Login")
-            if debugv ==1:
+            if debugv == 1:
                 print(e)
+            time.sleep(5)
     else:
         return 0
 
@@ -116,7 +159,14 @@ def view_balance():
     inp = input()
     match inp:
         case "2":
-            return
+            return 1
+        case "1":
+            clear()
+            if make_tracation() == 2:
+                clear()
+                make_tracation()
+            clear()
+            return 1
 
 def main_menu():
     style("Main Menu")
@@ -130,13 +180,29 @@ def main_menu():
     match inp:
         case "1":
             clear()
-            view_balance()
+            if view_balance() == 1:
+                clear()
+                return 1
+            else:
+                return 0
+        case "2":
+            match make_tracation():
+                case 1:
+                    clear()
+                    return 1
+                case 2:
+                    clear()
+                    make_tracation()
+                case _:
+                    return 0
 
 def main():
     clear()
-    if auth() ==1:
+    if auth() == 1:
         clear()
-        main_menu()
+        while main_menu() == 1:
+            1 == 1
+
     else:
         clear()
         return
